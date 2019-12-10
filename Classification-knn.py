0fde9e4b8798055edc2666b8f4f261bf60e1879a
abbahdab <52063067+abbahdab@users.noreply.github.com>
2019-12-09 20:57:45 -0500

    finished

--- Classification-knn.py
+++ Classification-knn.py
@@ -7,11 +7,16 @@ from sklearn import datasets
 from sklearn import metrics 
 import seaborn as sns 
 import numpy as np
+import TransformScript as ts
 
-crimes = pd.read_csv("Crime_Data_2010_2017_clean.csv")
+crimes = pd.DataFrame()
+crimes = ts.run(crimes, 'data/Crime_Data_2010_2017.csv')
 #print(crimes)
 #print(crimes.head())
 #print(crimes.shape)
+
+crimes = crimes[:len(crimes)//100]
+
 
 ''''Morning: 5am - 11:59am         0500 - 1159
     Afternoon: 12pm - 4:59pm       1200 - 1659
@@ -36,10 +41,12 @@ for i in crimesTime:
     elif i in range(1700, 2059):
         crimesTimeName.append("Evening")
         evenCount += 1
-    else:
+    elif i in range(2100, 2400) or i in range(0, 459):
         crimesTimeName.append("Night")
         nightCount += 1
-    
+    else:
+        crimesTimeName.append("NaN")
+
 crimes['Time of Day'] = crimesTimeName
 
 #print(len(crimes))
@@ -50,21 +57,20 @@ print('Crimes committed in the afternoon:',afterCount)
 print('Crimes committed in the evening:',evenCount)
 print('Crimes committed in the night:',nightCount)
 
-sns.countplot(crimes['Time of Day'], label = 'Crime Count')
+sns.countplot(crimes['Time of Day'], label = 'count')
+plt.xlabel('Time of Day')
+plt.ylabel('Crime Count')
+plt.title("Time of Day Crimes Occurred")
 plt.show()
 
-sns.countplot(crimes['Time of Day'], label = 'Crime Count')
-plt.show()
-
-crimes = crimes.dropna()
+#target = crimes.dropna()
 
 target = crimes.drop(columns = ['Time of Day','Time Occurred'])
 target.head()
 
-y = crimes['Time of Day']
+y = crimesTimeName
 
-X_train, X_test, y_train, y_test = train_test_split(target, y, test_size = 0.6)
-
+X_train, X_test, y_train, y_test = train_test_split(target, y, test_size = 0.2, random_state=0)
 #Import knearest neighbors Classifier model
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -81,7 +87,7 @@ print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 # Import confusion matrix and classification report
 print(confusion_matrix(y_test, y_pred))
-#print(classification_report(y_test, y_pred))
+print(classification_report(y_test, y_pred))
 
 k = 0
 for k in range(1,11):
@@ -89,3 +95,31 @@ for k in range(1,11):
     knn.fit(X_train, y_train)
     y_pred = knn.predict(X_test)
     print(k,"Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+
+crimes2 = pd.read_csv('data/Crime_Data_2010_2017.csv')
+crimes2 = crimes2[:len(crimes2)//100]
+weapons = crimes2['Weapon Used Code']
+
+nonViolentCount = 0
+violent = []
+violentCount = 0
+
+for i in weapons:
+    if i in range(100, 1000):
+        violentCount += 1
+        violent.append("Violent")
+    else:
+        nonViolentCount += 1
+        violent.append("Non-Violent")
+
+crimes['Violence'] = violent
+del crimes2
+
+violent = crimes[crimes['Violence'] == "Violent"]
+nonviolent = crimes[crimes['Violence'] == "Non-violent"]
+sns.countplot(x = "Time of Day", color = "green", data = crimes)
+sns.countplot(x = "Time of Day", hue = "Violence", palette = {"Violent": "#FF0000", "Non-Violent": "#3333FF"}, data = crimes)
+plt.ylabel("Crime Count")
+plt.title("Time of Day Violent and Non-Violent Crimes Occurred")
+plt.show()
